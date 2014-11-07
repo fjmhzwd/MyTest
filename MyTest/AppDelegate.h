161bdebf13b70e17e3b3b29b8ec79ffda46c4061
wayne <wayne@waynedeMacBook-Air.local>
2014-11-07 10:20:35 +0800

    Initial Commit

---
+++ MyTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTest
+//
+//  Created by wayne on 14-11-7.
+//  Copyright (c) 2014年 wayne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTest
+//
+//  Created by wayne on 14-11-7.
+//  Copyright (c) 2014年 wayne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
